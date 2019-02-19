acff088fb322f584ddc9a95ddc51dccb0d86d61c
ZF <413273029@qq.com>
2019-02-19 17:53:53 +0800

    轮播测试

---
+++ index.js
@@ -0,0 +1,39 @@
+var allButtons = $('#buttons > button')
+console.log(allButtons)
+
+for (var i = 0; i < allButtons.length; i++) {
+  $(allButtons[i]).on('click', function (x) {
+    console.log(x.currentTarget);
+    
+    var index = $(x.currentTarget).index()
+    var p = index * -300
+    $(x.currentTarget).addClass('red').siblings('.red').removeClass('red')
+    $('.images').css({
+      transform: 'translate(' + p + 'px)'
+    })
+    a = index
+  })
+}
+
+var a = 0
+var size = allButtons.length
+var timeId = setInterval(() => {
+  x(allButtons.eq(a % size))
+  a += 1
+}, 1000)
+
+$('.window').on('mouseenter', function () {
+  window.clearInterval(timeId)
+})
+
+$('.window').on('mouseleave', function () {
+  timeId = setInterval(() => {
+    x(allButtons.eq(a % size))
+    a += 1
+  }, 2000)
+})
+
+function x($button) {
+  $button.trigger('click')
+    .addClass('red').siblings('.red').removeClass('red')
+}
